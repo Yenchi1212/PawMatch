0cd9d05c61d09857a19ca4cc1936fb12544fccde
Yenchi1212 <142676491+Yenchi1212@users.noreply.github.com>
2025-12-05 16:03:53 +0000

    Added HomeScreen

---
+++ PawMatchApp/screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { View,Text,StyleSheet,TouchableOpacity, Alert } from 'react-native';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
+
+export default function HomeScreen({navigation}){
+
+    const handleLogout = async () => {
+        await signOut(auth);
+        Alert.alert("Logged out!");
+        navigation.replace("Login");
+    };
+
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.button}>Welcome to PawMatch!</Text>
+            <Text style = {styles.subtitle}>You are logged in.</Text>
+
+            <TouchableOpacity style ={styles.button} onPress={handleLogout}>
+                <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#555",
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: "#ff914d",
+    padding: 15,
+    borderRadius: 6,
+    width: "60%",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
